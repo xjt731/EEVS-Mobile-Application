84ebe850fa508c0abf82dd7774ad70e01e61ca61
gilbert777 <xjt674471772@mail.com>
2022-03-31 09:17:45 +1100

    First Commit

---
+++ app/src/main/java/com/example/eevs/Requirement.java
@@ -0,0 +1,14 @@
+package com.example.eevs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Requirement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requirement);
+    }
+}
